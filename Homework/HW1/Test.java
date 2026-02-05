20f16b6e33777043109355d8053e3f3f86a3fe33
Zohaib Muhammad <zohaibmuhammad8203@gmail.com>
2026-02-05 17:04:15 -0500

    Reset to default

--- Homework/HW1/Test.java
+++
@@ -1,7 +0,0 @@
-package Homework.HW1;
-
-public class Test {
-    public static void main(String [] args){
-        System.out.println("Hellow");
-    }
-}
--- Labs/Lab2/main.java
+++
@@ -1,7 +0,0 @@
-package Labs.Lab2;
-
-public class main {
-    public static void main(String [] args){
-        System.out.println("Hello");
-    }
-}
